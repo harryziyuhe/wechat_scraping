31161764a69d48ce96d239ef58185f5c803fd245
hfheziyu <zih028@ucsd.edu>
2024-06-06 12:58:55 -0700

    Update mitmexe.py

--- 01_code/mitmexe.py
+++ 01_code/mitmexe.py
@@ -1,12 +1,18 @@
 from mitmproxy import http, ctx
+from notifypy import Notify
+
 
 class Requests:
     def load(self, loader):
         ctx.options.http2 = False
     
     def request(self, flow: http.HTTPFlow) -> None:
+        notification = Notify()
+        notification.title = "Found"
+        notification.message = "Found it"
         target = {"weixin", "__biz=", "key="}
         if all(req in flow.request.url for req in target):
+            notification.send()
             f = open("Z:\key.txt", "w")
             url = flow.request.url
             try:
@@ -16,4 +22,5 @@ class Requests:
             f.close()
 
 if __name__=="__main__":
+
     addons = [Requests()]
