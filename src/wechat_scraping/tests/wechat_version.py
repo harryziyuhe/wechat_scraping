516c3b313553796154667dcf1c89ec7ee5d5bd70
hfheziyu <zih028@ucsd.edu>
2024-10-06 23:03:54 -0700

    Move wechat_version.py

---
+++ src/wechat_scraping/tests/wechat_version.py
@@ -0,0 +1,10 @@
+from src.wechat_scraping.virtualbox.param_retriever import setProxy, clearProxy
+import subprocess, os
+
+setProxy(f"127.0.0.1:8888")
+mitm_monitor_path = os.path.join(os.path.dirname(__file__), 'mitm_activity_monitor.py')
+try:
+    mitmprocess = subprocess.Popen(['mitmdump', '-s', mitm_monitor_path, '-p', '8888', '-q'])
+    mitmprocess.wait()
+finally:
+    clearProxy()
--- wechat_scraping/wechat_version.py
+++
@@ -1,9 +0,0 @@
-from retrieve_params import setProxy, clearProxy
-import subprocess
-
-setProxy(f"127.0.0.1:8888")
-try:
-    mitmprocess = subprocess.Popen(['mitmdump', '-s', 'Z:/mitm_monitor.py', '-p', '8888', '-q'])
-    mitmprocess.wait()
-finally:
-    clearProxy()
