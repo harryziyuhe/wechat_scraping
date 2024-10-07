0ddfd6831f05119836e4c9bf3b3fb21ffdbf2151
hfheziyu <zih028@ucsd.edu>
2024-10-07 08:35:38 -0700

    Update init files

---
+++ wechat_scraping/__init__.py
@@ -0,0 +1,13 @@
+from wechat_scraping.host.wechat_scraper import wechat_scraper
+from wechat_scraping.host.utils import renew_connection
+from wechat_scraping.virtualbox.param_retriever import proxyThread, setProxy, clearProxy, stopProxy, retrieve_params
+
+__all__ = [
+    'wechat_scraper',
+    'renew_connection',
+    'proxyThread',
+    'setProxy',
+    'clearProxy',
+    'stopProxy',
+    'retrieve_params'
+]
